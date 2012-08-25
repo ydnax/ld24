ffb2380d06297904a40b6b063d8b5b669e998d4f
xandy <gitmail.xandy@xoxy.net>
2012-08-25 12:36:46 +0200

    fix tileimage paths

--- src/ld24/LevelLoader.cpp
+++ src/ld24/LevelLoader.cpp
@@ -6,8 +6,8 @@ using namespace picppgl;
 LevelLoader::LevelLoader():
         row(0), column(0), maxlength(-1),
         levelmap(0,50),
-        sky(0,0), 
-        wall(0,0){}
+        sky("resources/gfx/sky.png", 50,50), 
+        wall("resources/gfx/wall.png",50,50){}
 
 
 LevelLoader::lvlInfo LevelLoader::parseLevel(string data){
